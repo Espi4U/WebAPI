7a62c86b0f7ba465a84bdd9db78a26bab4c60ccb
Espi4U <hingspeedusa369@gmail.com>
2020-01-19 21:37:51 +0200

    Add prototype DbContext

--- WebAPI/Models/APIModels/Family.cs
+++ WebAPI/Models/APIModels/Family.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebAPI.Models.APIModels
 {
     public class Family
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
     }
 }
---
+++ WebAPI/Models/FamilyFinanceContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models.APIModels;
+
+namespace WebAPI.Models
+{
+    public class FamilyFinanceContext
+    {
+        public IAsyncEnumerable<Family> Families { get; set; }
+        public IAsyncEnumerable<Person> Persons { get; set; }
+        public IAsyncEnumerable<Report> Reports { get; set; }
+        public IAsyncEnumerable<Purse> Purses { get; set; }
+        public IAsyncEnumerable<Purpose> Purposes { get; set; }
+        public IAsyncEnumerable<Category> Categories { get; set; }
+        public IAsyncEnumerable<ChangeMoney> ChangesMoney { get; set; }
+        public IAsyncEnumerable<Currency> Currencies { get; set; }
+    }
+}
