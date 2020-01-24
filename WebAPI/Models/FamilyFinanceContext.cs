e008b72dfc4456c720a05ffad936224ec09ec217
Espi4U <hingspeedusa369@gmail.com>
2020-01-24 21:22:32 +0200

    finish dbcontext and add mysql provider

--- WebAPI/Models/FamilyFinanceContext.cs
+++ WebAPI/Models/FamilyFinanceContext.cs
@@ -1,20 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
 using WebAPI.Models.APIModels;
 
 namespace WebAPI.Models
 {
-    public class FamilyFinanceContext
+    public class FamilyFinanceContext : DbContext
     {
-        public IAsyncEnumerable<Family> Families { get; set; }
-        public IAsyncEnumerable<Person> Persons { get; set; }
-        public IAsyncEnumerable<Report> Reports { get; set; }
-        public IAsyncEnumerable<Purse> Purses { get; set; }
-        public IAsyncEnumerable<Purpose> Purposes { get; set; }
-        public IAsyncEnumerable<Category> Categories { get; set; }
-        public IAsyncEnumerable<ChangeMoney> ChangesMoney { get; set; }
-        public IAsyncEnumerable<Currency> Currencies { get; set; }
+        public DbSet<Family> Families { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Report> Reports { get; set; }
+        public DbSet<Purse> Purses { get; set; }
+        public DbSet<Purpose> Purposes { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ChangeMoney> ChangesMoney { get; set; }
+        public DbSet<Currency> Currencies { get; set; }
+
+        public FamilyFinanceContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;UserId=root;Password=espi4upass;database=famillyfinance;");
+        }
     }
 }
