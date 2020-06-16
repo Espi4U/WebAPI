de9de0a44dba769b1d4a04b4d216b08c8f5705a0
Stefan <hingspeedusa369@gmail.com>
2020-06-16 21:55:10 +0300

    update cm list

---
+++ FamilyFinance/FamilyFinance/Converters/TypeCodeToColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FamilyFinance.Converters
+{
+    public class TypeCodeToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return value.ToString() == "I" ? "Green" : "Red";
+            }
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return "";
+        }
+    }
+}
